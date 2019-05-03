ccb3a07dd6b6cbd3e482a48c64e615340480a38e
Nicholas Wood <omfgnickybots@gmail.com>
2019-05-03 09:26:38 -0400

    Discord Bot 1.01

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const token = 'NTczNjAxNzU0MjE4ODg5MjM2.XMtQug.7HX4jWlBZeRsVkNUppu-HOYPEPg';
-
 const PREFIX = '~';
 
 bot.on('ready', () => {
     console.log("This bot is online!");
 });
-
 bot.on('message', message => {
 
     let args = message.content.substring(PREFIX.length).split(" ");
@@ -24,9 +21,22 @@ bot.on('message', message => {
             message.channel.sendMessage("Your mother!");
             break;
         case 'purge':
+
+            /**
+             * Delete
+             * Deletes the corresponding amount of messages based on user input
+             */
+        case 'delete':
             if(!args[1]) return message.reply('Define total number of lines');
             message.channel.bulkDelete(args[1]);
             break;
+
+            /**
+             * Reminder
+             * Creates a reminder with Day Month Year Time format
+             * Time is in military format
+             */
+
         case 'Reminder':
             if(!args[4]) return message.reply("Requries three arguements, Date Month Year Time. Please Enter time in military format.");
                 var day = args[1];
@@ -34,11 +44,34 @@ bot.on('message', message => {
                 var year = args[3];
                 var time = args[4];
                 return message.channel.send("Reminder Set for:" + " " + day + " " + month + " " + year + " " + time);
-            break;
-            case 'reminder':
-            if(!args[3]) return message.reply("Requries three arguements, Date Month Year");
-            break;
-    }
-})
+                break;
 
-bot.login(token);
+            /**
+             * Math
+             * Covers all 4 operations 
+             * 1 Addition, 2 Multiplication, 3 Division, 4 Subtraction
+             * Takes in three arguements -> (Operation, Value A, Value B)
+             */
+
+            case 'math':
+            if(!args[3]) return message.replay("Requires three arguements, 1 - Addition, 2 - Multiplication, 3 - Division, 4 - Subtraction. Then the two values, A and B");
+            var operation = args[1];
+            var a = arg[2];
+            var b = arg[3];
+
+            if(args[1] == 1){
+                return message.channel.send(a+b);
+            }
+            if(args[1] == 2){
+                return message.channel.send(a*b);
+            }
+            if(args[1] == 3){
+                return message.channel.send(a/b);
+            }
+            if(args[1] == 4){
+                return message.channel.send(a-b);
+            }
+        }       
+    })
+    
+    bot.login(token);
